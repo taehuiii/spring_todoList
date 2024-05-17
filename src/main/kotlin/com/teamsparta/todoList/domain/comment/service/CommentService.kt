ab45dcc68cb82dcafceefc8c14c2b13603a8dbb0
taehuiii <swanna18@naver.com>
2024-05-18 00:24:03 +0900

    [refactor] 안쓰는 파일 삭제

--- src/main/kotlin/com/teamsparta/todoList/domain/comment/service/CommentService.kt
+++
@@ -1,9 +0,0 @@
-package com.teamsparta.todoList.domain.comment.service
-
-import com.teamsparta.todoList.domain.comment.dto.AddCommentRequestDto
-import com.teamsparta.todoList.domain.comment.dto.CommentResponseDto
-import com.teamsparta.todoList.domain.comment.dto.UpdateCommentRequestDto
-
-interface CommentService {
-
-}
