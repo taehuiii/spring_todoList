946cadf1d304ef47c4e1492d9ff41741c401d671
taehuiii <swanna18@naver.com>
2024-05-21 20:56:14 +0900

    [ADD] User : UserResponseDto, SignUpRequestDto 추가 / application.yml 파일 highlight_sql추가

---
+++ src/main/kotlin/com/teamsparta/todoList/domain/user/dto/UserResponseDto.kt
@@ -0,0 +1,10 @@
+package com.teamsparta.todoList.domain.user.dto
+
+data class UserResponseDto(
+
+    val id: Long,
+    val userId : String,
+    val userName: String,
+    val nickname: String,
+
+)
